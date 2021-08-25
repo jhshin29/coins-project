3dee451077341bf94a090099fd32ee1a56f38d4e
jhshin29 <shinji03264@gmail.com>
2021-08-25 10:46:26 +0900

    [Fix] Update order cancel and refund money

--- src/view/StartView.java
+++ src/view/StartView.java
@@ -1,14 +1,10 @@
 package view;
 
-import org.junit.jupiter.api.Test;
-
 import controller.Controller;
-
 import model.dto.Orders;
 
 public class StartView {
 	
-	@Test
 	public static void main(String[] args) {
 		System.out.println("======== 3번 주문 조회 ========");
 		Controller.getOneOrder(3);
@@ -17,14 +13,14 @@ public class StartView {
 		Controller.getAllOrders();
 
 		System.out.println("======== 주문 추가 ==========");
-		Controller.addOrder("sjh0326", "Ripple", new Orders("2021-08-25", 3));
+		Controller.addOrder("sjh0326", "Doge", new Orders("2021-08-25", 5));
 		
 		System.out.println("======== 주문 수량 변경 ==========");
-		Controller.changeOrderQty(16, 50);
-		Controller.getOneOrder(16);
+		Controller.changeOrderQty(22, 5);
+		Controller.getOneOrder(22);
 		
-		System.out.println("======== 주문 삭제 ==========");	//예외처리 필요
-		Controller.deleteOrder(7);
+		System.out.println("======== 주문 삭제 ==========");
+		Controller.deleteOrder(22);
 		Controller.getAllOrders();
 	}
 	
